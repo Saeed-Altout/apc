4c73b5267efd996257d19f6a6077846b0b647ae4
Saeed Altout <saeedaltoutpro@gmail.com>
2025-04-06 23:49:07 +0300

    fix(constants): update DEFAULT_PATH to root for improved routing

--- config/constants.ts
+++ config/constants.ts
@@ -3,7 +3,7 @@ import { Home, Users, File, Stars, Settings } from "lucide-react";
 export const NAME_PROJECT = "Apc Prime";
 export const MAX_AGE_ACCESS_TOKEN = 30 * 24 * 60 * 60;
 export const MAX_AGE_REFRESH_TOKEN = 30 * 24 * 60 * 60;
-export const DEFAULT_PATH = "/dashboard";
+export const DEFAULT_PATH = "/";
 export const AUTH_LOGIN_REDIRECT = "/login";
 
 export const ROLES = [
